6ecfe14b974d6729912689b5f03ccc6bca419631
ksh <seongs70@naver.com>
2021-10-29 17:44:29 +0900

    woking

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,11 @@
+@extends('layouts.master')
+
+@section('content')
+    @include('partials.footer')
+@endsection
+
+{{--@section('script')--}}
+{{--    <script>--}}
+{{--        alert("저는 조각 뷰의 'script 섹션'");--}}
+{{--    </script>--}}
+{{--@endsection--}}
